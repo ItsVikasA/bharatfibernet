d386349b94bcbf60196add1fb0cf74bb9116c253
ItsVikasA <vikas.ambalazari@gmail.com>
2025-11-28 10:12:37 +0530

    Change testimonial background to 1.jpg

--- app/page.js
+++ app/page.js
@@ -335,7 +335,7 @@ const page = () => {
 <section 
   className="testimonial-section fix section-padding"
   style={{
-    backgroundImage: 'url("/assets/img/2.jpg")',
+    backgroundImage: 'url("/assets/img/1.jpg")',
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat'
